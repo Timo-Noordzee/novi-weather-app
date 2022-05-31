450b0420a6a98ec65e0cdb33e304f1425f14f77e
Timo Noordzee <timo_noordzee@hotmail.com>
2022-05-31 11:19:18 +0200

    Added react-router-dom@6 with 2 basic routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import {Route, Routes} from "react-router-dom";
 import "./App.css";
-import logo from "assets/logo.svg";
+
+const EmptyPage = ({title}) => {
+    return <h1>{title}</h1>;
+};
 
 function App () {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className="app">
+            <Routes>
+                <Route path="/" element={<EmptyPage title={"Home"} />} />
+                <Route path={"/login"} element={
+                    <EmptyPage title={"Login"} />} />
+            </Routes>
         </div>
     );
 }
